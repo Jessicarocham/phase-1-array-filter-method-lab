db12d1126497e8e7ead41626235a29e746f464f4
jessicarocham <jessicarocham18@hotmail.com>
2023-04-14 17:25:07 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+function findMatching(array, string) {
+  return array.filter(function (index) {
+    if (index.toLowerCase() === string.toLowerCase()) {
+      return index;
+    }
+  });
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter((element) => element.slice(0, 1) === string.slice(0, 1));
+}
+
+function matchName(object, string) {
+  return object.filter((driver) => driver.name === string);
+}
